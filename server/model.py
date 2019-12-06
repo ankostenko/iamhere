132cb4ef7ca0d0d280a1fe6450b55eb5727519ec
Alox1d <tom.9997@mail.ru>
2019-12-07 00:13:32 +0300

    added tag and its api

--- server/model.py
+++ server/model.py
@@ -1,12 +1,15 @@
 # _*_ coding = utf-8 _*_
-#!/usr/bin/env python3.6
+# !/usr/bin/env python3.6
 
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, LargeBinary, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
+from datetime import datetime, timedelta
+
 Base = declarative_base()
+
 
 class File(Base):
     __tablename__ = 'file'
@@ -17,6 +20,33 @@ class File(Base):
     def __init__(self, name, id=None):
         self.id = id
         self.name = name
+
+
+class Tag(Base):
+    __tablename__ = 'tag'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    x = Column(Integer, nullable=False)
+    y = Column(Integer, nullable=False)
+    file_type = Column(Integer, nullable=False)
+    stage_id = Column(Integer, ForeignKey('stage.id'))
+    created = Column(DateTime, default=datetime.now())
+    length_in_days = Column(Integer, default=1)  # add days by default
+
+    def __init__(self, name, x, y, file_type, stage_id, created, length_in_days, id=None):
+        self.id = id
+        self.name = name
+        self.x = x
+        self.y = y
+        self.file_type = file_type
+        self.stage_id = stage_id
+        self.created = created
+        self.length_in_days = length_in_days
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) if type(getattr(self, c.name)) != bytes else \
+            getattr(self, c.name).decode('utf-8') for c in self.__table__.columns}
 
 class Stage(Base):
     __tablename__ = 'stage'
@@ -33,8 +63,9 @@ class Stage(Base):
         self.building_id = building_id
 
     def as_dict(self):
-        return {c.name: getattr(self, c.name) if type(getattr(self, c.name)) != bytes else\
-               getattr(self, c.name).decode('utf-8') for c in self.__table__.columns}
+        return {c.name: getattr(self, c.name) if type(getattr(self, c.name)) != bytes else \
+            getattr(self, c.name).decode('utf-8') for c in self.__table__.columns}
+
 
 class Building(Base):
     __tablename__ = 'building'
@@ -49,8 +80,9 @@ class Building(Base):
         self.description = description
 
     def as_dict(self):
-        return {c.name: getattr(self, c.name) if type(getattr(self, c.name)) != bytes else\
-               getattr(self, c.name).decode('utf-8') for c in self.__table__.columns}
+        return {c.name: getattr(self, c.name) if type(getattr(self, c.name)) != bytes else \
+            getattr(self, c.name).decode('utf-8') for c in self.__table__.columns}
+
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///iamhere.db')
