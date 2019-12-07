b2a8d32a0ab860155f342fca1150ecc7cc8b3fc9
Alexey-ctrl <55793154+Alexey-ctrl@users.noreply.github.com>
2019-12-07 11:41:09 +0300

    Fill database test

--- server/test.py
+++ server/test.py
@@ -9,17 +9,38 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-# Добавим здание
-name = "ГО"
-description= "ул. Угличская, 36"
-building = Building(name, description)
-session.add(building)
-session.commit()
 
-# Добавим этаж
-stage = Stage(building_id = 1, name = "1", image_id="1")
-session.add(stage)
-session.commit()
-for stage in session.query(Stage).filter_by(building_id=1).all():
-    print(stage.as_dict())
+## Добавим здание
+# name = "ГО"
+# description = "ул. Угличская, 36"
+# building = Building(name, description)
+# session.add(building)
+# session.commit()
+#
+## Добавим этаж
+# stage = Stage(building_id=1, name="1", image_id="1")
+# session.add(stage)
+# session.commit()
+def create_building(name, descr):
+    building = Building(name, descr)
+    session.add(building)
+    session.commit()
 
+
+def create_stage(b_id, name, i_id):
+    stage = Stage(building_id=b_id, name=name, image_id=i_id)
+    session.add(stage)
+    session.commit()
+
+
+def delete_data():
+    session.query(Stage).filter_by(building_id=1).delete()
+    session.commit()
+    for i in range(1, session.query(Building).count() + 1):
+        session.query(Building).filter_by(id=i).delete()
+    session.commit()
+
+
+create_building("test", "уг 36")
+for i in range(1, 8):
+    create_stage(1, str(i), str(i))
