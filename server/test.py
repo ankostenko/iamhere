18aa74afb43821df3f792b3673496a954fe26335
Alexey-ctrl <55793154+Alexey-ctrl@users.noreply.github.com>
2019-12-07 16:31:51 +0300

    test.py

--- server/test.py
+++ server/test.py
@@ -1,8 +1,11 @@
 # _*_ coding = utf-8 _*_
-
+import os
+import uuid
+from shutil import copyfile
 from model import Building, Stage, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from server.model import File
 
 engine = create_engine('sqlite:///%s' % 'iamhere.db?charset=utf8', connect_args={'check_same_thread': False})
 Base.metadata.bind = engine
@@ -10,17 +13,15 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-## Добавим здание
-# name = "ГО"
-# description = "ул. Угличская, 36"
-# building = Building(name, description)
-# session.add(building)
-# session.commit()
-#
-## Добавим этаж
-# stage = Stage(building_id=1, name="1", image_id="1")
-# session.add(stage)
-# session.commit()
+def create_file(src):
+    dst = str(uuid.uuid4()) + '.' + src.split('.')[-1]
+    copyfile(src, os.path.join('files', dst))
+    file = File(dst)
+    session.add(file)
+    session.commit()
+    return file.id
+
+
 def create_building(name, descr):
     building = Building(name, descr)
     session.add(building)
@@ -34,13 +35,22 @@ def create_stage(b_id, name, i_id):
 
 
 def delete_data():
-    session.query(Stage).filter_by(building_id=1).delete()
-    session.commit()
+    for i in os.listdir("files"):
+        os.remove(f"files/{i}")
+    for i in range(1, session.query(Stage).count() + 1):
+        session.query(Stage).filter_by(id=i).delete()
     for i in range(1, session.query(Building).count() + 1):
-        session.query(Building).filter_by(id=i).delete()
-    session.commit()
+        session.query(Building).filter_by(name="Тензор").delete()
+    for i in range(1, session.query(File).count() + 1):
+        session.query(File).filter_by(id=i).delete()
+
 
 def run_test():
-    create_building("test", "уг 36")
+    delete_data()
+    create_building("Тензор", "Углическая 36")
     for i in range(1, 8):
-        create_stage(1, str(i), str(i))
+        create_stage(1, str(i), create_file(f"static/images/floor_{i}.png"))
+
+
+if __name__ == '__main__':
+    run_test()
