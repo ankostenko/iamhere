4535106d5bb69ee63b4ce7c900fc81b81825581f
sv.dokuchaev <sv.dokuchaev@tensor.ru>
2019-12-20 19:00:11 +0300

    Удалил библиотеки для оффлайн установки

--- install.cmd
+++ install.cmd
@@ -1,6 +1,7 @@
 REM We are installing dependencies
 cd libs
-for %%x in (dir *.whl) do py -3 -m pip install %%x
+for %%x in (dir *.whl) do py -3 -m pip install --no-index --find-links=%cd% %%x
+py -3 -m pip install --no-index --find-links=%cd% SQLAlchemy-1.3.12.tar.gz
 cd ..
 set PYTHONPATH=%cd%
-py -3 server/app.py
+py -3 server/app.py fill
---
+++ reload.cmd
@@ -0,0 +1,2 @@
+set PYTHONPATH=%cd%
+py -3 server/app.py fill
